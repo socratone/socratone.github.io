3c8e64be5ce55aeb6c3522ced0f7156eabbffbe0
socratone <gim2origin@gmail.com>
2021-02-19 01:02:40 +0900

    where 메서드를 빼먹었을 때 로컬에서 발생하는 오류 수정

--- _nuxt/static/1613651638/category/vuejs/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613651638",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcategory\u002Fvuejs",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"39693f1d"}}}}(null));
--- _nuxt/static/1613651638/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcategory","\u002Fstart-vuejs","\u002Ftypescript-basics","\u002Fcategory\u002Fvuejs","\u002Fcategory\u002Fdocker","\u002Fstart-docker","\u002Fcategory\u002Ftypescript"]})
