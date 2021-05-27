73579f31cd354bbd43eaf76d414e479147c1c206
socratone <gim2origin@gmail.com>
2021-05-28 01:04:07 +0900

    feat: builder 구현

--- builder.js
+++ builder.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const fse = require('fs-extra');
 const path = require('path');
 const dirTree = require('directory-tree');
@@ -6,37 +7,60 @@ const srcDir = path.join(__dirname, 'src');
 const destDir = path.join(__dirname, 'build');
 
 // build 폴더로 전체 복사
-// fse.copySync(srcDir, destDir, { overwrite: true }, function (err) {
-//   if (err) {
-//     console.error(err);
-//   } else {
-//     console.log('success!');
-//   }
-// });
+fse.copySync(srcDir, destDir, { overwrite: true }, function (err) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('success!');
+  }
+});
 
 // index.html 파일만 찾기
-
-const tree = dirTree(srcDir);
-// console.log(tree);
+const tree = dirTree(destDir);
 
 let htmls = [];
 
-findHTML(tree)
+findIndexHTML(tree);
 
-function findHTML(tree) {
+function findIndexHTML(tree) {
   function recursion(tree) {
     if (tree.name === 'index.html') {
-      htmls.push({ path: tree.path, name: tree.name })
+      htmls.push({ path: tree.path, name: tree.name });
     }
 
     if (tree.children) {
       for (let i = 0; i < tree.children.length; i++) {
-        recursion(tree.children[i])
+        recursion(tree.children[i]);
       }
     }
   }
 
-  recursion(tree)
+  recursion(tree);
 }
 
-console.log('htmls:', htmls)
+// console.log('htmls:', htmls)
+
+// common html 파일 불러오기
+const head = fs.readFileSync(
+  path.join(__dirname, 'src/common/head.html'),
+  'utf8'
+);
+const header = fs.readFileSync(
+  path.join(__dirname, 'src/common/header.html'),
+  'utf8'
+);
+
+// console.log('head:', head);
+// console.log('header:', header);
+
+// index.html 파일 텍스트 변환
+htmls.forEach((indexHTML) => {
+  let htmlText = fs.readFileSync(indexHTML.path, 'utf8');
+  htmlText = htmlText.replace('<!-- head -->', head);
+  htmlText = htmlText.replace('<!-- header -->', header);
+
+  fs.writeFile(indexHTML.path, htmlText, function (err) {
+    if (err) return console.log(err);
+  });
+});
+
