da20a65daec4facef34aa8c042588ae646aeffc4
socratone <gim2origin@gmail.com>
2021-05-27 23:55:42 +0900

    fix: copy 기능

--- builder.js
+++ builder.js
@@ -2,10 +2,10 @@ const fse = require('fs-extra')
 const path = require('path');
 
 const srcDir = path.join(__dirname, 'src')
-const destDir = path.join(__dirname, 'build')
+const destDir = path.join(__dirname)
 
 // To copy a folder or file
-fse.moveSync(srcDir, destDir, { overwrite: false }, function (err) {
+fse.copySync(srcDir, destDir, { overwrite: true }, function (err) {
   if (err) {
     console.error(err);
   } else {
