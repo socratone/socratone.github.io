e95add53f34bef6c96ea2e6d7e2da11b9fdf87e3
socratone <gim2origin@gmail.com>
2021-05-28 00:19:54 +0900

    feat: html 파일 경로, 이름 완전 탐색

--- builder.js
+++ builder.js
@@ -1,14 +1,42 @@
-const fse = require('fs-extra')
+const fse = require('fs-extra');
 const path = require('path');
+const dirTree = require('directory-tree');
 
-const srcDir = path.join(__dirname, 'src')
-const destDir = path.join(__dirname)
+const srcDir = path.join(__dirname, 'src');
+const destDir = path.join(__dirname, 'build');
 
-// To copy a folder or file
-fse.copySync(srcDir, destDir, { overwrite: true }, function (err) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('success!');
+// build 폴더로 전체 복사
+// fse.copySync(srcDir, destDir, { overwrite: true }, function (err) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log('success!');
+//   }
+// });
+
+// index.html 파일만 찾기
+
+const tree = dirTree(srcDir);
+// console.log(tree);
+
+let htmls = [];
+
+findHTML(tree)
+
+function findHTML(tree) {
+  function recursion(tree) {
+    if (tree.name === 'index.html') {
+      htmls.push({ path: tree.path, name: tree.name })
+    }
+
+    if (tree.children) {
+      for (let i = 0; i < tree.children.length; i++) {
+        recursion(tree.children[i])
+      }
+    }
   }
-});
+
+  recursion(tree)
+}
+
+console.log('htmls:', htmls)
