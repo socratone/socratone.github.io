ce74d1c1d3215350d5b8f8236c5275a6b00b51a7
Kiwon Kim <kiwon@elicer.com>
2023-08-13 23:47:05 +0900

    feat: update validateMarkdownMetadata helper to add return

--- helpers/markdown.ts
+++ helpers/markdown.ts
@@ -1,6 +1,12 @@
 import { BlogThumbnail } from 'constants/blog';
 import { isStringInEnum } from 'utils/enum';
 
+export type Metadata = {
+  title: string;
+  description: string;
+  thumbnail: BlogThumbnail;
+};
+
 export const validateMarkdownMetadata = (metadata: any) => {
   if (typeof metadata.title !== 'string') {
     throw new Error('Invalid title.');
@@ -13,4 +19,6 @@ export const validateMarkdownMetadata = (metadata: any) => {
   if (!isStringInEnum(metadata.thumbnail, BlogThumbnail)) {
     throw new Error('Invalid thumbnail.');
   }
+
+  return metadata as Metadata;
 };
