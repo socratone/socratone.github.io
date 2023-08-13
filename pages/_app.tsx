fb359b8b425810aaf41e5d3c15685aee0cf3874f
Kiwon Kim <kiwon@elicer.com>
2023-08-14 00:20:37 +0900

    feat: add meta to global

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import 'styles/globals.css';
 import { createTheme, ThemeProvider } from '@mui/material';
 import Container from '@mui/material/Container';
 import GlobalHeader from 'components/GlobalHeader/GlobalHeader';
+import Meta from 'components/Meta';
 import type { AppProps } from 'next/app';
 import { components } from 'theme/components';
 import { typography } from 'theme/typography';
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
         typography,
       })}
     >
+      <Meta title="소크라톤" description="프론트엔드 개발자 소크라톤 페이지" />
       <GlobalHeader />
       <Container sx={{ py: 2 }}>
         <Component {...pageProps} />
