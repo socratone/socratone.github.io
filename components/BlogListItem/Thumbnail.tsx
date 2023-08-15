6b0a9bc3599a191118d39bfffcda5d86bf029b52
Kiwon Kim <kiwon@elicer.com>
2023-08-15 17:57:37 +0900

    feat: add animation to regex item

--- components/BlogListItem/Thumbnail.tsx
+++ components/BlogListItem/Thumbnail.tsx
@@ -1,5 +1,7 @@
 import { Box, Typography } from '@mui/material';
 import { BlogThumbnail } from 'constants/blog';
+import { motion } from 'framer-motion';
+import { popUpContainer, popUpItem } from 'utils/animation';
 
 type ThumbnailProps = {
   type: BlogThumbnail;
@@ -7,7 +9,7 @@ type ThumbnailProps = {
 
 const REGEX_BLUE = '#2c5c97';
 const WHITE = '#fff';
-const THUMBNAIL_SIZE = 80;
+const THUMBNAIL_SIZE = '5rem'; // 80px
 
 const Thumbnail: React.FC<ThumbnailProps> = ({ type }) => {
   switch (type) {
@@ -17,12 +19,42 @@ const Thumbnail: React.FC<ThumbnailProps> = ({ type }) => {
           bgcolor={REGEX_BLUE}
           width={THUMBNAIL_SIZE}
           height={THUMBNAIL_SIZE}
+          fontSize={THUMBNAIL_SIZE}
           display="flex"
           justifyContent="center"
           alignItems="center"
         >
-          <Typography color={WHITE} fontSize={32}>
-            Reg
+          <Typography
+            component={motion.p}
+            {...popUpContainer}
+            color={WHITE}
+            fontSize="0.33em"
+            display="flex"
+          >
+            <Typography
+              component={motion.span}
+              {...popUpItem}
+              fontSize="inherit"
+              sx={{ display: 'block' }}
+            >
+              R
+            </Typography>
+            <Typography
+              component={motion.span}
+              {...popUpItem}
+              fontSize="inherit"
+              sx={{ display: 'block' }}
+            >
+              e
+            </Typography>
+            <Typography
+              component={motion.span}
+              {...popUpItem}
+              fontSize="inherit"
+              sx={{ display: 'block' }}
+            >
+              g
+            </Typography>
           </Typography>
         </Box>
       );
---
+++ utils/animation.ts
@@ -0,0 +1,54 @@
+/** popUpItem을 감싸서 item의 애니메이션이 순서대로 동작하게 한다. */
+export const popUpContainer = {
+  variants: {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  },
+  initial: 'hidden',
+  animate: 'show',
+  viewport: { once: true },
+};
+
+/** 애니메이션 item. popUpContainer와 같이 사용한다. */
+export const popUpItem = {
+  variants: {
+    hidden: { y: 30, opacity: 0 },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.4,
+        type: 'spring',
+        stiffness: 70,
+      },
+    },
+  },
+  viewport: { once: true },
+};
+
+export const popUp = (order = 0) => {
+  return {
+    initial: {
+      opacity: 0,
+      y: 30,
+    },
+    whileInView: {
+      y: 0,
+      opacity: 1,
+    },
+    transition: {
+      duration: 0.4,
+      type: 'spring',
+      stiffness: 70,
+      delay: 0.2 * order,
+    },
+    viewport: {
+      once: true,
+    },
+  };
+};
