9d58b02348401b8dac31906e7bb51dd84afc7d67
Kiwon Kim <kiwon@elicer.com>
2023-08-15 16:31:33 +0900

    remove: delete duplicate meta in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,13 @@
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import Head from 'next/head';
 
 const Home = () => {
   return (
-    <>
-      <Head>
-        <title>Socratone</title>
-        <meta name="description" content="Socratone's Development Blog" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Stack component="main">
-        <Typography color="text.primary">
-          반가워요 👋, Frontend Developer 김기원입니다!
-        </Typography>
-      </Stack>
-    </>
+    <Stack component="main">
+      <Typography color="text.primary">
+        반가워요 👋, Frontend Developer 김기원입니다!
+      </Typography>
+    </Stack>
   );
 };
 
