4eae7077b5b66d38ed53444fced446629eeddaea
Kiwon Kim <kiwon@elicer.com>
2023-08-24 02:46:28 +0900

    feat: add npm thumbnail

--- components/BlogListItem/Thumbnail.tsx
+++ components/BlogListItem/Thumbnail.tsx
@@ -1,12 +1,15 @@
 import { Box, Typography } from '@mui/material';
 import { BlogThumbnail } from 'constants/blog';
 import { motion } from 'framer-motion';
+import Image from 'next/image';
+import npmImage from 'public/images/blog/npm.webp';
 import { popUpContainer, popUpItem } from 'utils/animation';
 
 type ThumbnailProps = {
   type: BlogThumbnail;
 };
 
+const NPM_RED = '#cc3534';
 const REGEX_BLUE = '#2c5c97';
 const WHITE = '#fff';
 const THUMBNAIL_SIZE = '6rem'; // 80px
@@ -56,6 +59,37 @@ const Thumbnail: React.FC<ThumbnailProps> = ({ type }) => {
               g
             </Typography>
           </Typography>
+        </Box>
+      );
+
+    case BlogThumbnail.Npm:
+      return (
+        <Box
+          bgcolor={NPM_RED}
+          width={THUMBNAIL_SIZE}
+          height={THUMBNAIL_SIZE}
+          fontSize={THUMBNAIL_SIZE}
+        >
+          <Box
+            component={motion.div}
+            {...popUpContainer}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="100%"
+          >
+            <Box
+              component={motion.div}
+              {...popUpItem}
+              width="100%"
+              position="relative"
+              sx={{
+                aspectRatio: '300 / 116',
+              }}
+            >
+              <Image src={npmImage} alt="npm" fill />
+            </Box>
+          </Box>
         </Box>
       );
   }
