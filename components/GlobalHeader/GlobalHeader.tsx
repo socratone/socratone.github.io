868e58233084e41737bd9d5074842b2364744105
Kiwon Kim <kiwon@elicer.com>
2023-09-14 00:01:23 +0900

    feat: add github link to header

--- components/GlobalHeader/GlobalHeader.tsx
+++ components/GlobalHeader/GlobalHeader.tsx
@@ -1,6 +1,9 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
+import Link from 'next/link';
 
 import { GLOBAL_HEADER_HEIGHT } from './constants';
 import HeaderLink from './HeaderLink';
@@ -19,9 +22,18 @@ const GlobalHeader = () => {
       }}
     >
       <Container component="nav">
-        <Stack direction="row" spacing={2} height={GLOBAL_HEADER_HEIGHT}>
-          <HeaderLink href="/">Home</HeaderLink>
-          <HeaderLink href="/blogs">Blogs</HeaderLink>
+        <Stack direction="row" justifyContent="space-between">
+          <Stack direction="row" spacing={2} height={GLOBAL_HEADER_HEIGHT}>
+            <HeaderLink href="/">Home</HeaderLink>
+            <HeaderLink href="/blogs">Blogs</HeaderLink>
+          </Stack>
+          <Stack direction="row" alignItems="center">
+            <Link href="https://github.com/socratone" target="_blank">
+              <IconButton sx={{ mr: -1 }}>
+                <GitHubIcon />
+              </IconButton>
+            </Link>
+          </Stack>
         </Stack>
       </Container>
     </Box>
