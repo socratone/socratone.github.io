e85d22ae15a46774e644c7caec3655b16cf22c27
Kiwon Kim <kiwon@elicer.com>
2023-11-08 23:03:56 +0900

    feat: add tag for blogs filter

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -3,11 +3,13 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import BlogListItem from 'components/BlogListItem';
 import { GLOBAL_HEADER_HEIGHT } from 'components/GlobalHeader/constants';
+import { BlogTag } from 'constants/blog';
 import dayjs from 'dayjs';
 import { parseBlogTagForLabel } from 'helpers/blog';
 import { Metadata, validateMarkdownMetadata } from 'helpers/markdown';
 import { GetStaticProps, NextPage } from 'next';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { getFileNames } from 'utils/file';
 import { parseMarkdownFile } from 'utils/markdown';
 
@@ -15,6 +17,7 @@ type BlogsProps = {
   blogs: ({
     name: string;
   } & Metadata)[];
+  tags: BlogTag[];
 };
 
 export const getStaticProps: GetStaticProps<BlogsProps> = async () => {
@@ -23,32 +26,40 @@ export const getStaticProps: GetStaticProps<BlogsProps> = async () => {
     return fileName.substring(0, fileName.length - 3);
   });
 
-  const blogs = fileNamesWithoutExtension.map((name) => {
-    const { metadata } = parseMarkdownFile(`content/blogs/${name}.md`);
-    const validatedMetadata = validateMarkdownMetadata(metadata);
+  const blogs = fileNamesWithoutExtension
+    .map((name) => {
+      const { metadata } = parseMarkdownFile(`content/blogs/${name}.md`);
+      const validatedMetadata = validateMarkdownMetadata(metadata);
 
-    return {
-      name,
-      ...validatedMetadata,
-    };
-  });
+      return {
+        name,
+        ...validatedMetadata,
+      };
+    })
+    .toSorted((a, b) => {
+      const first = dayjs(a.createdAt);
+      const second = dayjs(b.createdAt);
+      if (first.isAfter(second)) return -1;
+      else if (first.isBefore(second)) return 1;
+      return 0;
+    });
 
-  blogs.sort((a, b) => {
-    const first = dayjs(a.createdAt);
-    const second = dayjs(b.createdAt);
-    if (first.isAfter(second)) return -1;
-    else if (first.isBefore(second)) return 1;
-    return 0;
-  });
+  const tags = [...new Set(blogs.map((blog) => blog.tag))].toSorted();
 
-  return { props: { blogs } };
+  return { props: { blogs, tags } };
 };
 
 const CONTAINER_PADDING_TOP = 16;
 const CATEGORY_WIDTH = 240;
 const GAP = 16;
 
-const Blogs: NextPage<BlogsProps> = ({ blogs }) => {
+const Blogs: NextPage<BlogsProps> = ({ blogs, tags }) => {
+  const router = useRouter();
+  const tagQuery = router.query.tag;
+  const filteredBlogs = tagQuery
+    ? blogs.filter((blog) => blog.tag === tagQuery)
+    : blogs;
+
   return (
     <Stack direction="row" gap={`${GAP}px`}>
       <Box
@@ -59,10 +70,21 @@ const Blogs: NextPage<BlogsProps> = ({ blogs }) => {
         <Stack
           position="sticky"
           top={GLOBAL_HEADER_HEIGHT + CONTAINER_PADDING_TOP}
+          gap={1}
         >
           <Link href="/blogs">
             <Typography>전체보기</Typography>
           </Link>
+          {tags.map((tag) => (
+            <Link key={tag} href={`/blogs?tag=${tag}`}>
+              <Typography
+                color="text.primary"
+                sx={{ textTransform: 'capitalize' }}
+              >
+                {parseBlogTagForLabel(tag)}
+              </Typography>
+            </Link>
+          ))}
         </Stack>
       </Box>
       <Stack
@@ -74,7 +96,7 @@ const Blogs: NextPage<BlogsProps> = ({ blogs }) => {
           lg: `calc(100% - ${CATEGORY_WIDTH + GAP}px)`,
         }}
       >
-        {blogs.map((blog) => (
+        {filteredBlogs.map((blog) => (
           <BlogListItem
             key={blog.name}
             title={blog.title}
