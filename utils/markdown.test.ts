ca6a46e5093533d684214ce2c8ccab35f918fc0c
Kiwon Kim <kiwon@elicer.com>
2023-12-16 17:31:08 +0900

    feat: add markdown sql code test

--- utils/markdown.test.ts
+++ utils/markdown.test.ts
@@ -1,15 +1,29 @@
 import { addCodeColorToHtml } from './markdown';
 
-test('class가 없는 code를 써도 정상 작동해야 한다. 뒤따라 나오는 class 있는 code에 영향을 주면 안 된다.', () => {
-  const html = `<p><code>나이</code>나</p>
-<pre><code class="language-python">from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-</code></pre>`;
+describe('addCodeColorToHtml', () => {
+  test('class가 없는 code를 써도 정상 작동해야 한다. 뒤따라 나오는 class 있는 code에 영향을 주면 안 된다.', () => {
+    const html = `<p><code>나이</code>나</p>
+  <pre><code class="language-python">from sklearn.compose import ColumnTransformer
+  from sklearn.preprocessing import OneHotEncoder
+  </code></pre>`;
 
-  const result = `<p><code>나이</code>나</p>
-<pre><code class="language-python"><span class="hljs-keyword">from</span> sklearn.compose <span class="hljs-keyword">import</span> ColumnTransformer
-<span class="hljs-keyword">from</span> sklearn.preprocessing <span class="hljs-keyword">import</span> OneHotEncoder
-</code></pre>`;
+    const result = `<p><code>나이</code>나</p>
+  <pre><code class="language-python"><span class="hljs-keyword">from</span> sklearn.compose <span class="hljs-keyword">import</span> ColumnTransformer
+  <span class="hljs-keyword">from</span> sklearn.preprocessing <span class="hljs-keyword">import</span> OneHotEncoder
+  </code></pre>`;
 
-  expect(addCodeColorToHtml(html)).toBe(result);
+    expect(addCodeColorToHtml(html)).toBe(result);
+  });
+
+  describe('sql', () => {
+    test(`<code> 내 '<'과 '>'을 나타내는 '&lt;'과 '&gt;'이 유지되어야 한다.`, () => {
+      const html = `<pre><code class="language-sql">create database &lt;이름&gt;;
+      </code></pre>`;
+
+      const result = `<pre><code class="language-sql"><span class="hljs-keyword">create</span> database <span class="hljs-operator">&lt;</span>이름<span class="hljs-operator">&gt;</span>;
+      </code></pre>`;
+
+      expect(addCodeColorToHtml(html)).toBe(result);
+    });
+  });
 });
