b47cf0178908a5784c215c760d9b23b64c39b32c
Kiwon Kim <kiwon@elicer.com>
2023-12-19 02:06:51 +0900

    feat: add generateTableOfContents

--- utils/markdown.ts
+++ utils/markdown.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import matter from 'gray-matter';
+import { JSDOM } from 'jsdom';
 import { marked } from 'marked';
 import path from 'path';
 
@@ -22,4 +23,32 @@ export const parseMarkdownFile = (fileName: string) => {
     metadata: data,
     content,
   };
+};
+
+export type TableOfContents = { level: number; text: string }[];
+
+export const generateTableOfContents = (html: string) => {
+  const { window } = new JSDOM(html);
+  const document = window.document;
+
+  const results: TableOfContents = [];
+
+  ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].forEach((headingTag) => {
+    const elements: NodeListOf<HTMLHeadingElement> =
+      document.querySelectorAll(headingTag);
+    elements.forEach((element) => {
+      element.classList.add('heading');
+    });
+  });
+
+  const headingElements: NodeListOf<HTMLHeadingElement> =
+    document.querySelectorAll('.heading');
+
+  headingElements.forEach((element) => {
+    const text = element.textContent?.trim() ?? '';
+    const level = parseInt(element.tagName.substring(1));
+    results.push({ level, text });
+  });
+
+  return results;
 };
