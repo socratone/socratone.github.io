2f5faacd10ee5d1be0048bc460eb7094cb541e66
Kiwon Kim <kiwon@elicer.com>
2024-02-11 15:31:06 +0900

    feat: add blogs seo

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,4 +1,9 @@
-import { BASE_URL } from 'constants/seo';
+import {
+  BASE_URL,
+  BLOGS_DESCRIPTION,
+  BLOGS_TITLE,
+  HOME_IMAGES,
+} from 'constants/seo';
 import dayjs from 'dayjs';
 import { validateMarkdownMetadata } from 'helpers/markdown';
 import type { Metadata } from 'next';
@@ -10,6 +15,15 @@ import BlogsPage from './blogs-page';
 
 export const metadata: Metadata = {
   metadataBase: new URL(BASE_URL),
+  title: BLOGS_TITLE,
+  description: BLOGS_DESCRIPTION,
+  openGraph: {
+    title: BLOGS_TITLE,
+    description: BLOGS_DESCRIPTION,
+    type: 'website',
+    siteName: 'Socratone',
+    images: HOME_IMAGES,
+  },
   alternates: {
     canonical: '/blogs',
   },
--- constants/seo.ts
+++ constants/seo.ts
@@ -1,6 +1,13 @@
+export const BASE_URL = 'https://socratone.github.io';
+
 /** Home page */
 
-export const BASE_URL = 'https://socratone.github.io';
-export const HOME_TITLE = '김기원 - 프론트엔드 개발자';
+export const HOME_TITLE = '김기원 프론트엔드 개발자';
 export const HOME_DESCRIPTION = '프론트엔드 개발자 김기원 페이지입니다.';
 export const HOME_IMAGES = `${BASE_URL}/images/resume/profile.webp`;
+
+/** Blogs page */
+
+export const BLOGS_TITLE = '김기원 개발 블로그';
+export const BLOGS_DESCRIPTION =
+  '김기원 개발 블로그입니다. 다음에 참고하기 위해 사용했던 기술 스택을 정리합니다. 빠르게 찾아볼 수 있도록 쉽고 간결하게 작성하려고 노력합니다.';
