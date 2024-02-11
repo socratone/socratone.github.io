c37c438e4ae11b23b0463ce79535f9515d46a492
Kiwon Kim <kiwon@elicer.com>
2024-02-11 15:17:47 +0900

    feat: replace home title with normal formatting

--- constants/seo.ts
+++ constants/seo.ts
@@ -1,4 +1,6 @@
+/** Home page */
+
 export const BASE_URL = 'https://socratone.github.io';
-export const HOME_TITLE = '김기원 | 프론트엔드 개발자';
+export const HOME_TITLE = '김기원 - 프론트엔드 개발자';
 export const HOME_DESCRIPTION = '프론트엔드 개발자 김기원 페이지입니다.';
 export const HOME_IMAGES = `${BASE_URL}/images/resume/profile.webp`;
