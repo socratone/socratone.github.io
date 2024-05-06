4889328c1eecbf6e1811bba7f71a20591366bbd5
Kiwon Kim <kiwon@elicer.com>
2024-05-07 00:34:53 +0900

    chore: setup mdx

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const createMDX = require('@next/mdx');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -6,6 +9,12 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: false,
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-module.exports = nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+module.exports = withMDX(nextConfig);
