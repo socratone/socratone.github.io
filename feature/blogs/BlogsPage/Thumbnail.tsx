b3d6be53ee6a5939e3657855d3805928aca342b1
Kiwon Kim <kiwon@elicer.com>
2024-06-09 23:30:20 +0900

    feat: add size and border radius to image

--- feature/blogs/BlogsPage/Thumbnail.tsx
+++ feature/blogs/BlogsPage/Thumbnail.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import styled from '@emotion/styled';
 import type { BlogThumbnail, LifehackThumbnail } from 'constants/blog';
 
 type ThumbnailProps = {
@@ -6,11 +7,19 @@ type ThumbnailProps = {
   name: BlogThumbnail | LifehackThumbnail;
 };
 
+const StyledImage = styled.img`
+  display: block;
+  border-radius: 8px;
+  width: 96px;
+  height: 96px;
+  object-fit: contain;
+`;
+
 const Thumbnail: React.FC<ThumbnailProps> = ({ type, name }) => {
   switch (type) {
     case 'blog':
       return (
-        <img
+        <StyledImage
           srcSet={`/images/blog/thumbnail/${name}-96w.webp, /images/blog/thumbnail/${name}-192w.webp 2x`}
           src={`/images/blog/thumbnail/${name}-192w.webp`}
           alt={name}
@@ -19,7 +28,7 @@ const Thumbnail: React.FC<ThumbnailProps> = ({ type, name }) => {
 
     case 'lifehack':
       return (
-        <img
+        <StyledImage
           srcSet={`/images/lifehack/thumbnail/${name}-96w.webp, /images/lifehack/thumbnail/${name}-192w.webp 2x`}
           src={`/images/lifehack/thumbnail/${name}-192w.webp`}
           alt={name}
